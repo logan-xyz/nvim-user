18045317feb6ced8de4b659e5ae7e6c01613c2db
logan271 <logan.luo.x@gmail.com>
2023-03-31 17:35:30 +0800

    add more language packs

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,14 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.json" },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
