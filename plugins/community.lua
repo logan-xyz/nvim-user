a3c762cc13f5c2c4742448a7d00edeff73c82905
logan271 <logan.luo.x@gmail.com>
2023-03-31 22:28:52 +0800

    feat: add neogen for code annotation

--- plugins/community.lua
+++ plugins/community.lua
@@ -14,7 +14,11 @@ return {
   { import = "astrocommunity.project.project-nvim" },
   -- motion
   { import = "astrocommunity.motion.nvim-surround" },
-  -- motion
+  -- editing-support
+  -- A better annotation generator. Supports multiple languages and annotation conventions.
+  { import = "astrocommunity.editing-support.neogen" },
+  -- { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  -- { import = "astrocommunity.editing-support.refactoring-nvim" },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
