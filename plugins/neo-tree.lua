3775396db9caf7c2b19a8e60fe714c2f63c4487e
logan271 <logan.luo.x@gmail.com>
2023-03-31 17:49:47 +0800

    feat(neo-tree): adjust tree view indent

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -1,6 +1,9 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    default_component_configs = {
+      indent = { padding = 0, indent_size = 2 },
+    },
     filesystem = {
       filtered_items = {
         visible = true, -- when true, they will just be displayed differently than normal items
