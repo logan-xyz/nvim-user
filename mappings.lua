508e7c0ca59ef0bc794faa4d8e809060fe02344e
logan271 <logan.luo.x@gmail.com>
2023-03-31 14:02:30 +0800

    Open Alpha Automatically When No More Buffers

--- mappings.lua
+++ mappings.lua
@@ -11,6 +11,15 @@ return function(local_vim)
   local_vim.n["<leader>h"] = false
   local_vim.n["<leader>b-"] = local_vim.n["<leader>b\\"]
   local_vim.n["<leader>b\\"] = false
+  local_vim.n["<leader>c"] = {
+    -- Open Alpha Automatically When No More Buffers
+    function()
+      local bufs = vim.fn.getbufinfo { buflisted = true }
+      require("astronvim.utils.buffer").close(0)
+      if require("astronvim.utils").is_available "alpha-nvim" and not bufs[2] then require("alpha").start(true) end
+    end,
+    desc = "Close buffer",
+  }
 
   return local_vim
 end
---
+++ plugins/lsp/on_attach.lua
@@ -0,0 +1,3 @@
+return function(client, bufnr)
+  -- run some code when a lanague server attaches
+end
