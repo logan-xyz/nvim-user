ef429312cd179b882eb76941d40dbb980ab0e166
logan271 <logan.luo.x@gmail.com>
2023-03-31 17:43:41 +0800

    feat(neo-tree): show dotfiles and ignored file by default

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -2,6 +2,12 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true, -- when true, they will just be displayed differently than normal items
+        hide_gitignored = false,
+        hide_hidden = false,
+        hide_dotfiles = false,
+      },
       window = {
         mappings = {
           ["<leftrelease>"] = "open",
