b1a33f4e33b0387cbeccbbb27f4ee1e10f6ac739
logan271 <logan.luo.x@gmail.com>
2023-03-31 20:55:01 +0800

    feat: add project-nvim

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,7 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  -- lancuage packs
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.typescript" },
@@ -9,6 +10,8 @@ return {
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.json" },
+  -- project
+  { import = "astrocommunity.project.project-nvim" },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
