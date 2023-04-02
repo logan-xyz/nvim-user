bad292cf5fbc75007531b2b4fcc50721b74a0f1e
logan271 <logan.luo.x@gmail.com>
2023-04-02 10:05:18 +0800

    feat(gitsigns): enable git current line blame

---
+++ plugins/gitsigns.lua
@@ -0,0 +1,13 @@
+return {
+  -- https://github.com/lewis6991/gitsigns.nvim
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+      delay = 200,
+      ignore_whitespace = false,
+    },
+  },
+}
