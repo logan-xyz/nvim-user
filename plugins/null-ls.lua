fa465b71bb244589cc0704c69a5647be19f6d42d
logan271 <logan.luo.x@gmail.com>
2023-04-02 15:24:15 +0800

    feat: add gitsigns code_actions source

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -17,6 +17,7 @@ return {
       null_ls.builtins.completion.spell,
       null_ls.builtins.diagnostics.cspell,
       null_ls.builtins.code_actions.cspell,
+      null_ls.builtins.code_actions.gitsigns,
     }
 
     return config -- return final config table
